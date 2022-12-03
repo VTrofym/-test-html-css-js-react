afa39955c9cc93f669754b480a374bcb9878334c
VTrofym <v.trof@icloud.com>
2022-12-03 16:26:15 +0200

    start

---
+++ test/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello!!!
+      <div className="first"></div>
+      <div className="second"></div>
+    </div>
+  );
+}
+
+// const obj = {
+//   x: 10,
+//   y: 20,
+// };
+// const prop = Object.keys(obj).reverse().join();
+// obj.xy = 30;
+// obj.yx = 40;
+// obj.x10y20 = 50;
+// obj.y20x10 = 60;
+// const result = obj[prop];
+// console.log(result);
+
+export default App;
